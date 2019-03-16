36366291975901139c2e522ab3ae949b1f1b6993
hzhkidding <1126766942@qq.com>
2019-03-16 08:54:50 +0800

    first

---
+++ src/main/java/com/nowcoder/async/EventType.java
@@ -0,0 +1,17 @@
+package com.nowcoder.async;
+
+/**
+ * Created by nowcoder on 2016/7/30.
+ */
+public enum EventType {
+    LIKE(0),
+    COMMENT(1),
+    LOGIN(2),
+    MAIL(3),
+    FOLLOW(4),
+    UNFOLLOW(5);
+
+    private int value;
+    EventType(int value) { this.value = value; }
+    public int getValue() { return value; }
+}
